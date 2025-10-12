656f8ce9403f7d38a0eca6e8c9661e972ee2ff51
Akira Takagi <tkgakrwork@gmail.com>
2025-10-12 12:21:44 +0900

    4.96 colorOfTile をクラスに移す続き

--- index.ts
+++ index.ts
@@ -464,18 +464,7 @@ function drawMap(g: CanvasRenderingContext2D) {
 
 
 function colorOfTile(g: CanvasRenderingContext2D, x: number, y: number) {
-  if (map[y][x].isFlux())
-    g.fillStyle = "#ccffcc";
-  else if (map[y][x].isUnbreakable())
-    g.fillStyle = "#999999";
-  else if (map[y][x].isStone() || map[y][x].isFallingStone())
-    g.fillStyle = "#0000cc";
-  else if (map[y][x].isBox() || map[y][x].isFallingBox())
-    g.fillStyle = "#8b4513";
-  else if (map[y][x].isKey1() || map[y][x].isLock1())
-    g.fillStyle = "#ffcc00";
-  else if (map[y][x].isKey2() || map[y][x].isLock2())
-    g.fillStyle = "#00ccff";
+  map[y][x].color(g)
 }
 
 function drawPlayer(g: CanvasRenderingContext2D) {
