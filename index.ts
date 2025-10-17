dc2c060df8d3734b04390236567baf43148530ac
Akira Takagi <tkgakrwork@gmail.com>
2025-10-17 23:17:43 +0900

    5.21 StoneクラスとFallingStoneクラスをまとめる

--- index.ts
+++ index.ts
@@ -129,36 +129,6 @@ class Stone implements Tile {
   isBoxy() { return false; }
 }
 
-class FallingStone implements Tile {
-  private falling: boolean;
-  constructor(falling: boolean) {
-    this.falling = falling;
-  }
-  isAir() { return false; }
-  isFallingStone() { return this.falling; }
-  isFallingBox() { return false; }
-  isLock1() { return false; }
-  isLock2() { return false; }
-  draw(g: CanvasRenderingContext2D, x: number, y: number) {
-    g.fillStyle = "#0000cc";
-    g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-  }
-  moveHorizontal(dx: number) {
-    if (this.isFallingStone() === false) {
-      if (map[playery][playerx + dx + dx].isAir()
-        && !map[playery + 1][playerx + dx].isAir()) {
-        map[playery][playerx + dx + dx] = this;
-        moveToTile(playerx + dx, playery);
-      }
-    } else if (this.isFallingStone() === true) {
-    }
-  }
-  moveVertical(dy: number) {
-  }
-  isStony() { return true; }
-  isBoxy() { return false; }
-}
-
 class Box implements Tile {
   isAir() { return false; }
   isFallingStone() { return false; }
@@ -329,7 +299,7 @@ function transformTile(tile: RawTile) {
     case RawTile.PLAYER: return new Player();
     case RawTile.UNBREAKABLE: return new Unbreakable();
     case RawTile.STONE: return new Stone(false);
-    case RawTile.FALLING_STONE: return new FallingStone(true);
+    case RawTile.FALLING_STONE: return new Stone(true);
     case RawTile.BOX: return new Box();
     case RawTile.FALLING_BOX: return new FallingBox();
     case RawTile.FLUX: return new Flux();
@@ -402,7 +372,7 @@ function updateMap() {
 function updateTile(y: number, x: number) {
   if (map[y][x].isStony()
     && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new FallingStone(true);
+    map[y + 1][x] = new Stone(true);
     map[y][x] = new Air();
   } else if (map[y][x].isBoxy()
     && map[y + 1][x].isAir()) {
