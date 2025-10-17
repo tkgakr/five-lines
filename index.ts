b08f962a91bd46ce864af946ad50a2dba49de09f
Akira Takagi <tkgakrwork@gmail.com>
2025-10-17 23:15:38 +0900

    5.19 wip

--- index.ts
+++ index.ts
@@ -101,8 +101,8 @@ class Player implements Tile {
 
 class Stone implements Tile {
   private falling: boolean;
-  constructor() {
-    this.falling = false;
+  constructor(falling: boolean) {
+    this.falling = falling;
   }
   isAir() { return false; }
   isFallingStone() { return this.falling; }
@@ -131,8 +131,8 @@ class Stone implements Tile {
 
 class FallingStone implements Tile {
   private falling: boolean;
-  constructor() {
-    this.falling = true;
+  constructor(falling: boolean) {
+    this.falling = falling;
   }
   isAir() { return false; }
   isFallingStone() { return this.falling; }
@@ -328,8 +328,8 @@ function transformTile(tile: RawTile) {
     case RawTile.AIR: return new Air();
     case RawTile.PLAYER: return new Player();
     case RawTile.UNBREAKABLE: return new Unbreakable();
-    case RawTile.STONE: return new Stone();
-    case RawTile.FALLING_STONE: return new FallingStone();
+    case RawTile.STONE: return new Stone(false);
+    case RawTile.FALLING_STONE: return new FallingStone(true);
     case RawTile.BOX: return new Box();
     case RawTile.FALLING_BOX: return new FallingBox();
     case RawTile.FLUX: return new Flux();
@@ -402,14 +402,14 @@ function updateMap() {
 function updateTile(y: number, x: number) {
   if (map[y][x].isStony()
     && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new FallingStone();
+    map[y + 1][x] = new FallingStone(true);
     map[y][x] = new Air();
   } else if (map[y][x].isBoxy()
     && map[y + 1][x].isAir()) {
     map[y + 1][x] = new FallingBox();
     map[y][x] = new Air();
   } else if (map[y][x].isFallingStone()) {
-    map[y][x] = new Stone();
+    map[y][x] = new Stone(false);
   } else if (map[y][x].isFallingBox()) {
     map[y][x] = new Box();
   }
