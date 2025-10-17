0deb612dc71122ec0b81a89651a6175e319dded8
Akira Takagi <tkgakrwork@gmail.com>
2025-10-17 22:29:59 +0900

    5.7 wip

--- index.ts
+++ index.ts
@@ -110,10 +110,12 @@ class Stone implements Tile {
     g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
   moveHorizontal(dx: number) {
-    if (map[playery][playerx + dx + dx].isAir()
-      && !map[playery + 1][playerx + dx].isAir()) {
-      map[playery][playerx + dx + dx] = this;
-      moveToTile(playerx + dx, playery);
+    if (true) {
+      if (map[playery][playerx + dx + dx].isAir()
+        && !map[playery + 1][playerx + dx].isAir()) {
+        map[playery][playerx + dx + dx] = this;
+        moveToTile(playerx + dx, playery);
+      }
     }
   }
   moveVertical(dy: number) {
@@ -133,6 +135,7 @@ class FallingStone implements Tile {
     g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
   moveHorizontal(dx: number) {
+    if (true) { }
   }
   moveVertical(dy: number) {
   }
