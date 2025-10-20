7cade4dbcfa52ef0035c076cbfb93552f3230663
Akira Takagi <tkgakrwork@gmail.com>
2025-10-20 23:36:54 +0900

    5.54

--- index.ts
+++ index.ts
@@ -391,9 +391,8 @@ function updateTile(y: number, x: number) {
     && map[y + 1][x].isAir()) {
     map[y + 1][x] = new Box(new Falling());
     map[y][x] = new Air();
-  } else if (map[y][x].isFallingStone()) {
-    map[y][x].rest();
-  } else if (map[y][x].isFallingBox()) {
+  } else if (map[y][x].isFallingStone()
+    || map[y][x].isFallingBox()) {
     map[y][x].rest();
   }
 }
