66d56334b13aaff899b9d4e91707d87428ffd7cf
Akira Takagi <tkgakrwork@gmail.com>
2025-10-24 23:35:03 +0900

    5.87 fallStrategy パターンの導入

--- index.ts
+++ index.ts
@@ -108,8 +108,8 @@ class Resting implements FallingState {
 
 class Stone implements Tile {
   private fallStrategy: FallStrategy;
-  constructor(private falling: FallingState) {
-    this.fallStrategy = new FallStrategy();
+  constructor(falling: FallingState) {
+    this.fallStrategy = new FallStrategy(falling);
   }
   isAir() { return false; }
   isLock1() { return false; }
@@ -119,25 +119,19 @@ class Stone implements Tile {
     g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
   moveHorizontal(dx: number) {
-    this.falling.moveHorizontal(this, dx);
+    this.fallStrategy.getFalling().moveHorizontal(this, dx);
   }
   moveVertical(dy: number) {
   }
   update(x: number, y: number) {
-    if (map[y + 1][x].isAir()) {
-      this.falling = new Falling();
-      map[y + 1][x] = this;
-      map[y][x] = new Air();
-    } else if (this.falling.isFalling()) {
-      this.falling = new Resting();
-    }
+    this.fallStrategy.update(this, x, y);
   }
 }
 
 class Box implements Tile {
   private fallStrategy: FallStrategy;
-  constructor(private falling: FallingState) {
-    this.fallStrategy = new FallStrategy();
+  constructor(falling: FallingState) {
+    this.fallStrategy = new FallStrategy(falling);
   }
   isAir() { return false; }
   isLock1() { return false; }
@@ -147,18 +141,12 @@ class Box implements Tile {
     g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
   moveHorizontal(dx: number) {
-    this.falling.moveHorizontal(this, dx);
+    this.fallStrategy.getFalling().moveHorizontal(this, dx);
   }
   moveVertical(dy: number) {
   }
   update(x: number, y: number) {
-    if (map[y + 1][x].isAir()) {
-      this.falling = new Falling();
-      map[y + 1][x] = this;
-      map[y][x] = new Air();
-    } else if (this.falling.isFalling()) {
-      this.falling = new Resting();
-    }
+    this.fallStrategy.update(this, x, y);
   }
 }
 
@@ -231,6 +219,19 @@ class Lock2 implements Tile {
 }
 
 class FallStrategy {
+  constructor(
+    private falling: FallingState
+  ) { }
+  getFalling() { return this.falling; }
+  update(tile: Tile, x: number, y: number) {
+    if (map[y + 1][x].isAir()) {
+      this.falling = new Falling();
+      map[y + 1][x] = tile;
+      map[y][x] = new Air();
+    } else if (this.falling.isFalling()) {
+      this.falling = new Resting();
+    }
+  }
 
 }
 
