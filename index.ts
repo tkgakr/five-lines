41b1365b2726ba1f12d2050da5eb0f7e302450c1
Akira Takagi <tkgakrwork@gmail.com>
2025-10-25 00:00:43 +0900

    5.91 fallingへの代入処理を共通化

--- index.ts
+++ index.ts
@@ -224,12 +224,10 @@ class FallStrategy {
   ) { }
   getFalling() { return this.falling; }
   update(tile: Tile, x: number, y: number) {
-    if (map[y + 1][x].isAir()) {
-      this.falling = new Falling();
+    this.falling = map[y + 1][x].isAir() ? new Falling() : new Resting();
+    if (this.falling.isFalling()) {
       map[y + 1][x] = tile;
       map[y][x] = new Air();
-    } else {
-      this.falling = new Resting();
     }
   }
 
