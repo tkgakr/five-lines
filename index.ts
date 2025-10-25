4d503736096d7564a4cad13b153a5f91d8459e12
Akira Takagi <tkgakrwork@gmail.com>
2025-10-25 16:27:35 +0900

    5.137 実装からインターフェースを抽出する

--- index.ts
+++ index.ts
@@ -308,7 +308,7 @@ function transformMap() {
 let inputs: Input[] = [];
 
 function removeLock1() {
-  let shouldRemove = new RemoveStrategy();
+  let shouldRemove = new RemoveLock1();
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
       if (shouldRemove.check(map[y][x])) {
@@ -318,10 +318,14 @@ function removeLock1() {
   }
 }
 
-class RemoveStrategy {
+class RemoveLock1 implements RemoveStrategy {
   check(tile: Tile) {
     return tile.isLock1();
   }
+}
+
+interface RemoveStrategy {
+  check(tile: Tile): boolean;
 }
 
 function removeLock2() {
