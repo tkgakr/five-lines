fd548568757a261d3f5db20b08292186d62d7a56
Akira Takagi <tkgakrwork@gmail.com>
2025-10-31 19:17:47 +0900

    wip6.78 transformMap を Map クラスに移す

--- index.ts
+++ index.ts
@@ -306,6 +306,15 @@ class Map {
   private map: Tile[][];
   getMap() { return this.map; }
   setMap(map: Tile[][]) { this.map = map; }
+  transform() {
+    this.map = new Array(rawMap.length);
+    for (let y = 0; y < rawMap.length; y++) {
+      this.map[y] = new Array(rawMap[y].length);
+      for (let x = 0; x < rawMap[y].length; x++) {
+        this.map[y][x] = transformTile(rawMap[y][x]);
+      }
+    }
+  }
 
 }
 let map = new Map();
@@ -328,15 +337,6 @@ function transformTile(tile: RawTile) {
     case RawTile.KEY2: return new Key(BLUE_KEY);
     case RawTile.LOCK2: return new LockTile(BLUE_KEY);
     default: assertExhausted(tile);
-  }
-}
-function transformMap(map: Map) {
-  map.setMap(new Array(rawMap.length));
-  for (let y = 0; y < rawMap.length; y++) {
-    map.getMap()[y] = new Array(rawMap[y].length);
-    for (let x = 0; x < rawMap[y].length; x++) {
-      map.getMap()[y][x] = transformTile(rawMap[y][x]);
-    }
   }
 }
 
@@ -448,7 +448,7 @@ function gameLoop(map: Map) {
 }
 
 window.onload = () => {
-  transformMap(map);
+  map.transform();
   gameLoop(map);
 };
 
