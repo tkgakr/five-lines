a36bc2b23554ed1cf984183c1c75ade0fc10d507
Akira Takagi <tkgakrwork@gmail.com>
2025-11-03 16:45:54 +0900

    6.108 enum RawTile の代替用に RAW_TILESを作成

--- index.ts
+++ index.ts
@@ -32,6 +32,21 @@ class RawTile2 {
   private constructor(private value: RawTileValue) { }
 }
 
+const RAW_TILES = [
+  RawTile2.AIR,
+  RawTile2.FLUX,
+  RawTile2.UNBREAKABLE,
+  RawTile2.PLAYER,
+  RawTile2.STONE,
+  RawTile2.FALLING_STONE,
+  RawTile2.BOX,
+  RawTile2.FALLING_BOX,
+  RawTile2.KEY1,
+  RawTile2.LOCK1,
+  RawTile2.KEY2,
+  RawTile2.LOCK2
+];
+
 enum RawTile {
   AIR,
   FLUX,
