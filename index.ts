c8d8903f0cdaef7cba563cc49073a64188caf802
Akira Takagi <tkgakrwork@gmail.com>
2025-11-03 17:18:46 +0900

    transformTile メソッドのインライン化

--- index.ts
+++ index.ts
@@ -392,7 +392,7 @@ class Map {
     for (let y = 0; y < rawMap.length; y++) {
       this.map[y] = new Array(rawMap[y].length);
       for (let x = 0; x < rawMap[y].length; x++) {
-        this.map[y][x] = transformTile(RAW_TILES[rawMap[y][x]]);
+        this.map[y][x] = RAW_TILES[rawMap[y][x]].transform();
       }
     }
   }
@@ -448,9 +448,6 @@ class Map {
 }
 function assertExhausted(x: never): never {
   throw new Error("Unexpected object: " + x);
-}
-function transformTile(tile: RawTile2) {
-  return tile.transform();
 }
 
 let inputs: Input[] = [];
